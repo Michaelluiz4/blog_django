6cde14898021f08a226c85d606fb0001d0a25b0e
Michael <michaelsb065@gmail.com>
2024-11-19 01:59:47 -0300

    Atualização/Modificação

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog import models
 
 # Register your models here.
+
+@admin.register(models.Post)
+class PostBlog(admin.ModelAdmin):
+    list_display = 'tittle',
+    ordering = '-id',
+    search_fields = 'tittle',
+    list_filter = 'date',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_display_links = 'tittle',
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    tittle = models.CharField(max_length=50)
+    content = models.TextField()
+    date = models.DateField(default=timezone.now)
+
+
+    def __str__(self):
+        return f'{self.tittle}'
